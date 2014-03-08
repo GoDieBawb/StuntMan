457d856e137664e7cecbdbd1fcfbe4c13d619700
NegroBob <meancraft@hotmail.com>
2014-03-08 03:10:59 -0800

    Made Game Reasonably Difficult and Comment Party
    Made Game Difficult by switching shake
    Comment Party in SceneManager

--- Stuntman/src/mygame/SceneManager.java
+++ Stuntman/src/mygame/SceneManager.java
@@ -32,6 +32,8 @@ public class SceneManager extends AbstractAppState {
   public    Node                   wallNode;
   public    int                    difficulty;
   private   ArrayList<TextureKey>  textArray;
+  private   int                    neg;
+  private   int                    negDelay;
     
   @Override
   public void initialize(AppStateManager stateManager, Application app) {
@@ -47,10 +49,11 @@ public class SceneManager extends AbstractAppState {
     rootNode.attachChild(wallNode);
     initMaterials();
     createFloor();
-    //createRightWall();
-    //createLeftWall();
+    negDelay = 0;
+    neg = 1;
     }
   
+  //Creating a list of textures for the buildings
   public void initMaterials(){
     textArray = new ArrayList();
     TextureKey key1 = new TextureKey("Textures/Buildings/CasaRosa.jpg");
@@ -63,8 +66,7 @@ public class SceneManager extends AbstractAppState {
     textArray.add(key4);
     }
   
-  //Create the Floor
-  
+  //Creates the Floor
   public void createFloor(){
     Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
     mat.setColor("Color", ColorRGBA.DarkGray);
@@ -78,20 +80,26 @@ public class SceneManager extends AbstractAppState {
     }
   
   //Create the wall on the right side
-  
   public void createRightWall(){
-    System.out.println("Right Wall Created");
+      
+    //Generate the materials and random numbers
     Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
     Random texRand = new Random();
     int texSel = texRand.nextInt(4);
+    
+    //choose and add a random texture
     TextureKey key = textArray.get(texSel);
     key.setGenerateMips(true);
     Texture tex = assetManager.loadTexture(key);
     mat.setTexture("ColorMap", tex);
+    
+    //Create the Geometry
     Box a = new Box(5f, 5f, 50f);
     Geometry wall = new Geometry("Right Wall", a);
     wall.setMaterial(mat);
     wallNode.attachChild(wall);
+    
+    //Place the Geometry in a hallway fashion
     Random rand = new Random();
     float distance = rand.nextInt(7) + 9; 
     wall.setLocalTranslation(-distance, 0f, -101f);
@@ -99,20 +107,26 @@ public class SceneManager extends AbstractAppState {
   
   
   //Create the wall on the left side
-  
   public void createLeftWall(){
-    System.out.println("Left Wall Created");
+    
+    //generate the materials and random numbers
     Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
     Random texRand = new Random();
     int texSel = texRand.nextInt(4);
+    
+    //choose and add a random texture
     TextureKey key = textArray.get(texSel);
     key.setGenerateMips(true);
     Texture tex = assetManager.loadTexture(key);
     mat.setTexture("ColorMap", tex);
+    
+    //Create the Geometry
     Box a = new Box(5f, 5f, 50f);
     Geometry wall = new Geometry("Left Wall", a);
     wall.setMaterial(mat);
     wallNode.attachChild(wall);
+    
+    //Place the Geometry in a hallway fashion
     Random rand = new Random();
     float distance = rand.nextInt(7) + 9; 
     wall.setLocalTranslation(distance, 0f, -101f);
@@ -120,9 +134,12 @@ public class SceneManager extends AbstractAppState {
   
   @Override
   public void update(float tpf){
+      
+  //Check if the player is dead.
   Player player = stateManager.getState(PlayerManager.class).currentPlayer;
   if (!player.isDead){
     
+    //Fixes the hallways when there is nothing there. Fixes the Bug, also used to start  
     if(wallNode.getChildren().isEmpty()){
       System.out.println("Bug Fixed!");
       createLeftWall();
@@ -134,15 +151,36 @@ public class SceneManager extends AbstractAppState {
     for(int i = 0; i < wallNode.getChildren().size(); i++){
       Geometry geom = (Geometry) wallNode.getChild(i);
       Random rand = new Random();
-      float shake = rand.nextInt(5);
-      Vector3f moveDir = new Vector3f(1f * shake, 0f, difficulty);
+      
+      //Sets the amplitude of the shakes
+      int shaker = rand.nextInt(5);
+      
+      //Keeps the shake reasonable
+      int shakeShift = difficulty/30;
+      if(shakeShift < 1)
+      shakeShift = 1;
+      
+      //Determines the direction of the shake
+      if (negDelay == 2000){
+        neg = rand.nextInt(2) - 1;
+        if (neg == 0)
+        neg = 1;
+        negDelay = 0;
+        } else{
+        negDelay++;
+        }
+      
+      //Actually Does the moving of the geometry
+      float shake = shakeShift * shaker * neg;
+      Vector3f moveDir = new Vector3f(shake, 0f, difficulty);
       geom.setLocalTranslation(geom.getLocalTranslation().addLocal(moveDir.mult(tpf)));
       int distance = (int) geom.getLocalTranslation().distance(player.model.getLocalTranslation().add(0f, 0f, -50f));
       
       
-      //If the distance is exactly 50 and there is less than for walls, create two more walls, BUGGED!!!!
-      
-      if (distance == 55 && wallNode.getChildren().size() < 4) {
+      //If the distance is exactly 50 and there is less than for walls, create two more walls.
+      if (distance == 53 && wallNode.getChildren().size() < 4) {
+        
+        //creates the right wall, which is the second wall, sets boolean to false, to assure walls generate in pairs
         if (lastWall){
           createRightWall();
           player.score++;
@@ -151,14 +189,14 @@ public class SceneManager extends AbstractAppState {
           System.out.println(player.score);
           lastWall = false;
           }
+        
+        //Creates the left wall, sets boolean to true, allowing the right wall to be created
         if (!lastWall){
           createLeftWall();
           lastWall = true;
           }
         }
 
-
-      
       //If greater than 120 remove the wall, allowing for new walls to be created
       if(distance > 120){
         geom.removeFromParent();
